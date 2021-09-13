e99889e76807b731d0f82acc795ff5defc32a8d0
arctavares <cosilk@hotmail.com>
2021-09-13 18:59:45 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const text = document.getElementById('meme-text');
 const input = document.getElementById('text-input');
 
-input.addEventListener('input', (event) => {
-    text.innerHTML = input.value;
+input.addEventListener('input', () => {
+  text.innerHTML = input.value;
+});
+
+const insert = document.getElementById('meme-insert');
+const image = document.getElementById('meme-image');
+
+const reader = new FileReader();
+
+insert.addEventListener('change', (event) => {
+  const { files } = event.target;
+  const file = files[0];
+  reader.readAsDataURL(file);
+
+  reader.addEventListener('load', (event) => {
+    image.src = event.target.result;
+    image.alt = file.name;
+  });
 });
