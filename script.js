3d64be8b1b58bada2de79bd839f7de155af34b85
Esau Matias <esaufreirematias@gmail.com>
2021-09-14 18:17:38 -0300

    resoluçao do requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const inputText = document.querySelector('#text-input');
-const inputFile = document.querySelector('#file-input');
+const inputFile = document.querySelector('#meme-insert');
 let textMeme = document.querySelector('#meme-text');
-let imageMeme = document.querySelector('#frame');
-const buttonClick = document.querySelector('button');
+let imageMeme = document.querySelector('#meme-image');
 
 inputText.addEventListener('keyup', textGerador);
 
 function textGerador() {
- let valueText = inputText.value;
- textMeme.innerHTML = valueText;
- preview()
+ textMeme.innerHTML = inputText.value;
 }
 
-function preview() {
-  imageMeme.src = inputFile.value;
+inputFile.addEventListener('input', imageGerador);
+
+function imageGerador() {
+  imageMeme.src = URL.createObjectURL(inputFile.files[0]);
 }
