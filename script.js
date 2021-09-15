caced3ff74439ae56ae6b4e36babed1222af3681
gui <guilhaugusto05@gmail.com>
2021-09-14 23:55:43 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const image = document.getElementById('meme-image');
 const imageInsert = document.getElementById('meme-insert');
+const imageContainer = document.getElementById('meme-image-container');
+
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
 
 function setMemeText() {
   if (inputText.value.length > 60) {
@@ -16,5 +21,17 @@ function setImage(e) {
   image.src = URL.createObjectURL(e.target.files[0]);
 }
 
+function changeFrameColor(e) {
+  const element = e.target;
+  const newColor = window.getComputedStyle(element).getPropertyValue('background-color');
+  const newBorder = window.getComputedStyle(element).getPropertyValue('border');
+  imageContainer.style.backgroundColor = `${newColor}`;
+  imageContainer.style.border = `${newBorder}`;
+}
+
 inputText.addEventListener('keyup', setMemeText);
 imageInsert.addEventListener('change', setImage);
+
+fireButton.addEventListener('click', changeFrameColor);
+waterButton.addEventListener('click', changeFrameColor);
+earthButton.addEventListener('click', changeFrameColor);
