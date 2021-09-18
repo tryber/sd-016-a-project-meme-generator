2904983768a4b412aee5dc7a36590eaac6f9189c
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-09-18 19:45:52 -0300

    adicionado requisito 7 e finalizado

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const memeImgContainer = document.getElementById('meme-image-container');
 const fireButton = document.getElementById('fire');
 const waterButton = document.getElementById('water');
 const earthButton = document.getElementById('earth');
+const memeSuggenstion = document.getElementById('memes-suggestions');
 
-//https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+// https://developer.mozilla.org/en-US/docs/Web/API/FileReader
 const reader = new FileReader();
 
 function updateText() {
@@ -17,20 +18,20 @@ function updateText() {
 textInput.addEventListener('keyup', updateText);
 
 function imageLoad() {
-  //pega o arquivo enviado e guarda nesta variavel
-  const userImage = memeInsert.files[0]
+  // pega o arquivo enviado e guarda nesta variavel
+  const userImage = memeInsert.files[0];
 
-  //Usa a função do objeto leitor de arquivos, que lê o arquivo, e consegue reaproveita-lo para usar o arquivo como uma URL
+  // Usa a função do objeto leitor de arquivos, que lê o arquivo, e consegue reaproveita-lo para usar o arquivo como uma URL
   reader.readAsDataURL(userImage);
 
-  //Após leitura da imagem (evento load), a função de callback define o valor do src da imagem de prévia com o valor do resultado da leitura do leitor de arquivos
-  reader.addEventListener('load', function (load) {
+  // Após leitura da imagem (evento load), a função de callback define o valor do src da imagem de prévia com o valor do resultado da leitura do leitor de arquivos
+  reader.addEventListener('load', (load) => {
     memeImage.src = load.target.result;
   });
 }
 /* imageLoad retirada a ideia do site https://codepen.io/vanessametonini/pen/zLmzNY */
 
-//https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
 memeInsert.addEventListener('change', imageLoad);
 
 function applyFireBorder() {
@@ -48,5 +49,6 @@ function applyEarthBorder() {
 }
 earthButton.addEventListener('click', applyEarthBorder);
 
-
-
+memeSuggenstion.addEventListener('click', (event) => {
+  memeImage.src = event.target.src;
+});
