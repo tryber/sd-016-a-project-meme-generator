2d54996bc6fc18e52d9e6a0d9b10ae29f1f5983d
Adran <adran.carnavale@gmail.com>
2021-09-18 12:46:07 -0300

    finishes requirement 2, applied lint

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const textInput = document.querySelector('#text-input');
 const imageContainer = document.querySelector('.image-container');
+const parag = document.querySelector('.meme-description');
+const memeInsert = document.querySelector('.meme-insert');
+const memeImage = document.querySelector('.meme-image');
 
 function generatesText() {
   const text = textInput.value;
-  imageContainer.innerText = text;
-  return 0;
+  parag.innerText = text;
 }
 
 textInput.addEventListener('keyup', generatesText);
+
+function generatesImage() {
+  // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+  memeImage.src = URL.createObjectURL(memeInsert.files[0]);
+  memeImage.style.width = `${476}px`;
+  memeImage.style.height = `${500}px`;
+  imageContainer.appendChild(memeImage);
+}
+
+memeInsert.addEventListener('change', generatesImage);
