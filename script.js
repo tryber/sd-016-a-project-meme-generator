d06ccfaaca6c1a3a75c63f6e9396245bef14f244
Adran <adran.carnavale@gmail.com>
2021-09-19 14:55:30 -0300

    adds requirement 6

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 const textInput = document.querySelector('#text-input');
-const imageContainer = document.querySelector('.image-container');
-const parag = document.querySelector('.meme-description');
-const memeInsert = document.querySelector('.meme-insert');
-const memeImage = document.querySelector('.meme-image');
+const memeInsert = document.querySelector('#meme-insert');
+const memeImageContainer = document.querySelector('#meme-image-container');
+const memeImage = document.querySelector('#meme-image');
+const memeText = document.querySelector('#meme-text');
 
-function generatesText() {
+function createsText() {
   const text = textInput.value;
-  parag.innerText = text;
+  memeText.innerText = text;
+  memeImageContainer.appendChild(memeText);
 }
 
-textInput.addEventListener('keyup', generatesText);
+textInput.addEventListener('keyup', createsText);
 
-function generatesImage() {
-  // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+function createsImage() {
+  // https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
   memeImage.src = URL.createObjectURL(memeInsert.files[0]);
-  memeImage.style.width = `${476}px`;
-  memeImage.style.height = `${500}px`;
-  imageContainer.appendChild(memeImage);
+  memeImage.style.width = '500px';
+  memeImage.style.height = '500px';
+  memeImageContainer.appendChild(memeImage);
 }
 
-memeInsert.addEventListener('change', generatesImage);
+memeInsert.addEventListener('change', createsImage);
+
+const buttons = {
+  fire() {
+    memeImageContainer.style.setProperty('--main-border', '3px dashed red');
+  },
+  water() {
+    memeImageContainer.style.setProperty('--main-border', '5px double blue');
+  },
+  earth() {
+    memeImageContainer.style.setProperty('--main-border', '6px groove green');
+  },
+};
+
+const fireBtn = buttons.fire;
+const waterBtn = buttons.water;
+const earthBtn = buttons.earth;
+
+document.addEventListener('click', (event) => {
+  const trigger = event.target.id;
+  if (trigger === 'fire') {
+    fireBtn();
+  } else if (trigger === 'water') {
+    waterBtn();
+  } else if (trigger === 'earth') {
+    earthBtn();
+  } else {
+    return 1;
+  }
+});
