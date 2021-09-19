121dbad797d34b1a5898344696654c0763d03bae
joao <jgsspinelli92@gmail.com>
2021-09-19 08:15:45 -0300

    entrega do requisito 4

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 function printText() {
   const text = document.getElementById('text-input').value;
   let printedText = document.getElementById('meme-text')
-  console.log(text.length)
-  if (text.length <= 60) {
     printedText.innerText = text;
-  }
 }
 document.getElementById('text-input').addEventListener('keyup', printText);
 
-function printImage() {
-  const parentElement = document.getElementById('meme-image-container')
-  const uploadedImage = document.getElementById('upload-image').value;
-  let printedImage = document.createElement('img');
-  console.log(uploadedImage)
+function previewImage() {
+  let file = document.getElementById('meme-insert').files
+  if (file.length > 0) {
+    let fileReader = new FileReader();
 
-  printedImage.src = uploadedImage.src;
-  parentElement.appendChild(printedImage);
-}
-document.getElementById('send-image').addEventListener('click', printImage)
-
-
-
-function limitedCharacters() {
-  const text = document.getElementById('text-input').value;
-  const textLiberated = [];
-
-  for (let index = 0; index <= 60; index += 1) {
-    textLiberated.push(text[index]);
+    fileReader.onload = function (event) {
+      document.getElementById('meme-image').setAttribute('src', event.target.result)
+    }
+    fileReader.readAsDataURL(file[0]);
   }
-  return textLiberated.join(',')
 }
+document.getElementById('meme-insert').addEventListener('change', previewImage)
