3b1bdcfb5bc1d241164715fcb01295bff747dc1f
Veidz <joaovictorveidz@gmail.com>
2021-09-22 04:53:12 -0300

    BONUS -> 7 - Adicionando memes defaults, para serem selecionados

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ memeInsert.addEventListener('change', () => {
   fileReader.onload = () => {
     memeImage.src = fileReader.result;
   }
+
+  fileReader.readAsDataURL(memeInsert.files[0]);
 })
 
 function createBorderButtons() {
@@ -66,7 +68,6 @@ let fireBorderButton = document.getElementById('fire');
 let waterBorderButton = document.getElementById('water');
 let earthBorderButton = document.getElementById('earth');
 
-
 function fireBorderStyle() {
   imageContainer.style.border = fireBorderButton.style.border;
 }
@@ -82,3 +83,58 @@ function earthBorderStyle() {
 fireBorderButton.onclick = fireBorderStyle;
 waterBorderButton.onclick = waterBorderStyle;
 earthBorderButton.onclick = earthBorderStyle;
+
+function createDefaultMemes() {
+  let meme1 = document.createElement('img');
+  let meme2 = document.createElement('img');
+  let meme3 = document.createElement('img');
+  let meme4 = document.createElement('img');
+
+  meme1.src = './imgs/meme1.png';
+  meme1.id = 'meme-1';
+  meme1.className = 'default-memes';
+
+  meme2.src = './imgs/meme2.png';
+  meme2.id = 'meme-2';
+  meme2.className = 'default-memes';
+
+  meme3.src = './imgs/meme3.png';
+  meme3.id = 'meme-3';
+  meme3.className = 'default-memes';
+
+  meme4.src = './imgs/meme4.png';
+  meme4.id = 'meme-4';
+  meme4.className = 'default-memes';
+
+  mainContainer.appendChild(meme1);
+  mainContainer.appendChild(meme2);
+  mainContainer.appendChild(meme3);
+  mainContainer.appendChild(meme4);
+}
+
+createDefaultMemes();
+
+let meme1 = document.getElementById('meme-1');
+let meme2 = document.getElementById('meme-2');
+let meme3 = document.getElementById('meme-3');
+let meme4 = document.getElementById('meme-4');
+function handleMeme1() {
+  memeImage.src = meme1.src;
+}
+
+function handleMeme2() {
+  memeImage.src = meme2.src;
+}
+
+function handleMeme3() {
+  memeImage.src = meme3.src;
+}
+
+function handleMeme4() {
+  memeImage.src = meme4.src;
+}
+
+meme1.onclick = handleMeme1;
+meme2.onclick = handleMeme2;
+meme3.onclick = handleMeme3;
+meme4.onclick = handleMeme4;
