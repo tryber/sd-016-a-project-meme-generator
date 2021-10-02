5fe4d25e7da5b60bbb360db8f538d9c0f930e265
baky2708 <flavioisaque123@gmail.com>
2021-10-02 12:39:49 -0300

    06 requeriment - Complete

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// const container = document.querySelector('#meme-image-container');
+const container = document.querySelector('#meme-image-container');
 const txtInput = document.querySelector('#text-input');
 const text = document.querySelector('#meme-text');
 const imageInput = document.querySelector('#meme-insert');
 const image = document.querySelector('#meme-image');
+const btnRed = document.querySelector('#fire');
+const btnWater = document.querySelector('#water');
+const btnEarth = document.querySelector('#earth');
 
 txtInput.addEventListener('input', () => {
   text.innerHTML = txtInput.value;
@@ -12,4 +15,16 @@ imageInput.addEventListener('change', (event) => {
   if (event.target.files.length > 0) {
     image.src = URL.createObjectURL(event.target.files[0]);
   }
+});
+
+btnRed.addEventListener('click', () => {
+  container.style.border = '3px dashed red';
+});
+
+btnWater.addEventListener('click', () => {
+  container.style.border = '5px double blue';
+});
+
+btnEarth.addEventListener('click', () => {
+  container.style.border = '6px groove green';
 });
