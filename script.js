a881977b0fa5c9f121990c8192fee31693341815
baky2708 <flavioisaque123@gmail.com>
2021-10-02 11:54:10 -0300

    02 requeriment - Complete

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // const container = document.querySelector('#meme-image-container');
-const text = document.querySelector('#meme-text');
 const txtInput = document.querySelector('#text-input');
+const text = document.querySelector('#meme-text');
+const imageInput = document.querySelector('#meme-insert');
+const image = document.querySelector('#meme-image');
 
 txtInput.addEventListener('input', () => {
   text.innerHTML = txtInput.value;
+});
+
+imageInput.addEventListener('change', (event) => {
+  if (event.target.files.length > 0) {
+    image.src = URL.createObjectURL(event.target.files[0]);
+  }
 });
