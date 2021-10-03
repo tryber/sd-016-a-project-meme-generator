7af4de4da817f48bb77bea3d5085da920a8f3c6b
baky2708 <flavioisaque123@gmail.com>
2021-10-03 14:37:20 -0300

    project - Complete

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const image = document.querySelector('#meme-image');
 const btnRed = document.querySelector('#fire');
 const btnWater = document.querySelector('#water');
 const btnEarth = document.querySelector('#earth');
+const btnThunder = document.querySelector('#thunder');
+const photo = document.querySelectorAll('.photo');
 
 txtInput.addEventListener('input', () => {
   text.innerHTML = txtInput.value;
@@ -28,3 +30,23 @@ btnWater.addEventListener('click', () => {
 btnEarth.addEventListener('click', () => {
   container.style.border = '6px groove green';
 });
+
+btnThunder.addEventListener('click', () => {
+  container.style.border = '6px groove yellow';
+});
+
+function resetSelect() {
+  for (let i = 0; i < photo.length; i += 1) {
+    photo[i].classList.remove('selected');
+    photo[i].classList.add('no-selected');
+  }
+}
+
+for (let i = 0; i < photo.length; i += 1) {
+  photo[i].addEventListener('click', (event) => {
+    resetSelect();
+    event.target.classList.remove('no-selected');
+    event.target.classList.add('selected');
+    image.src = event.target.src;
+  });
+}
