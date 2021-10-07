c2eb625ab7d7e2f36ba10c0f1e22d01333524fb8
Laura <lahfumagalli@gmail.com>
2021-10-06 22:25:58 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const inputText = document.getElementById('text-input');
 const writeText = document.getElementById('meme-text');
+const imgSelect = document.getElementById('meme-insert');
+const imgUppada = document.getElementById('meme-image');
 
 inputText.addEventListener('input', () => {
-  console.log('oi')
   writeText.innerHTML = inputText.value;
+})
+
+imgSelect.addEventListener('change', (event) => {
+    imgUppada.src = URL.createObjectURL(event.target.files[0]);
 })
