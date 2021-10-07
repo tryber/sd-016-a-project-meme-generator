3840a5881e8551d2b5b616218118ea8a8ef18103
Laura <lahfumagalli@gmail.com>
2021-10-07 00:29:21 -0300

    100%

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ inputText.addEventListener('input', () => {
 
 imgSelect.addEventListener('change', (event) => {
     imgUppada.src = URL.createObjectURL(event.target.files[0]); // Flavio code-review + https://developer.mozilla.org/pt-BR/docs/Web/API/URL/createObjectURl
+    // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
 })
 
 const btnFire = document.querySelector('#fire');
@@ -27,3 +28,24 @@ btnWater.addEventListener('click', () => {
 btnEarth.addEventListener('click', () => {
   imgContainer.style.border = '6px groove green';
 });
+
+const btn1 = document.getElementById('meme-1');
+const btn2 = document.getElementById('meme-2');
+const btn3 = document.getElementById('meme-3');
+const btn4 = document.getElementById('meme-4');
+
+btn1.addEventListener('click', () => {
+  imgUppada.src = btn1.src;
+})
+
+btn2.addEventListener('click', () => {
+  imgUppada.src = btn2.src;
+})
+
+btn3.addEventListener('click', () => {
+  imgUppada.src = btn3.src;
+})
+
+btn4.addEventListener('click', () => {
+  imgUppada.src = btn4.src;
+})
