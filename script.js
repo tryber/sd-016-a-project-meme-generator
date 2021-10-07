0186225974d169a964ffc25515c6533c32a15b07
Laura <lahfumagalli@gmail.com>
2021-10-06 23:00:41 -0300

    requisito 6

--- script.js
+++ script.js
@@ -11,4 +11,19 @@ imgSelect.addEventListener('change', (event) => {
     imgUppada.src = URL.createObjectURL(event.target.files[0]); // Flavio code-review + https://developer.mozilla.org/pt-BR/docs/Web/API/URL/createObjectURl
 })
 
+const btnFire = document.querySelector('#fire');
+const btnWater = document.querySelector('#water');
+const btnEarth = document.querySelector('#earth');
+const imgContainer = document.querySelector('#meme-image-container');
 
+btnFire.addEventListener('click', () => {
+  imgContainer.style.border = '3px dashed red';
+});
+
+btnWater.addEventListener('click', () => {
+  imgContainer.style.border = '5px double blue';
+});
+
+btnEarth.addEventListener('click', () => {
+  imgContainer.style.border = '6px groove green';
+});
